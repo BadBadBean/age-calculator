8163be57ed84d61ad72ec251ce463c88a8f285cb
BadBadBean <mallet.delphine@hotmail.fr>
2025-02-10 00:12:02 +0100

    light/dark mode variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./style/main.scss";
 import Form from "./components/Form";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr == "light" ? "dark" : "light"))
+  }
 
   return(
-    <div className='container'>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className='container' id={theme}>
+      {/* <ToggleButton onChange={toggleTheme} checked={theme === "dark"} /> */}
       <div className='card'>
         <Form />
       </div>
     </div>
+    </ThemeContext.Provider>
   )
 
 }
