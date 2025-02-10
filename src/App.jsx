981dfc517604d22bf000e5c63bfb8223a4f3def1
Delphine <mallet.delphine@hotmail.fr>
2025-02-10 01:05:26 +0100

    edit style

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import "./style/main.scss";
 import Form from "./components/Form";
-import { createContext, useState } from "react";
-
-export const ThemeContext = createContext(null)
 
 function App() {
-  const [theme, setTheme] = useState("light")
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr == "light" ? "dark" : "light"))
-  }
-
-  return(
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-    <div className='container' id={theme}>
+  return (
+    <div className="container">
       {/* <ToggleButton onChange={toggleTheme} checked={theme === "dark"} /> */}
-      <div className='card'>
+      <div className="card">
         <Form />
       </div>
     </div>
-    </ThemeContext.Provider>
-  )
-
+  );
 }
 
-export default App
+export default App;
